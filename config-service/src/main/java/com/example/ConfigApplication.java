739693900cf6a23d87191322f32ef261da18776a
cockmake <2359016829@qq.com>
2023-09-12 15:37:50 +0800

    config-service待完成

--- config-service/src/main/java/com/example/ConfigApplication.java
+++ config-service/src/main/java/com/example/ConfigApplication.java
@@ -3,8 +3,10 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableConfigServer
 public class ConfigApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class, args);
